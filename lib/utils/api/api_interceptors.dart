72f36fa0b260d0ddb8c01606862c4100fd425918
iptoday <wangdong1221@outlook.com>
2022-12-16 13:25:14 +0800

    add checksum

--- lib/utils/api/api_interceptors.dart
+++ lib/utils/api/api_interceptors.dart
@@ -2,13 +2,17 @@
  * @Author: iptoday wangdong1221@outlook.com
  * @Date: 2022-09-01 16:10:49
  * @LastEditors: iptoday wangdong1221@outlook.com
- * @LastEditTime: 2022-11-16 17:18:09
+ * @LastEditTime: 2022-12-16 13:18:22
  * @FilePath: /tikfans2/lib/utils/api/api_interceptors.dart
  * 
  * Copyright (c) 2022 by iptoday wangdong1221@outlook.com, All Rights Reserved. 
  */
 import 'dart:convert';
 import 'dart:io';
+// ignore: depend_on_referenced_packages
+import 'package:convert/convert.dart';
+// ignore: depend_on_referenced_packages
+import 'package:crypto/crypto.dart';
 
 import 'package:dio/dio.dart';
 import 'package:steel_crypt/steel_crypt.dart';
@@ -31,16 +35,28 @@ class ApiInterceptors extends InterceptorsWrapper {
       lang = AppConfig.instance.box.read('x-app-lang');
     }
     options.headers['x-app-lang'] = lang;
+    String ts = '${DateTime.now().millisecondsSinceEpoch}';
     params['route'] =
         options.path.startsWith('/') ? options.path.substring(1) : options.path;
     params['deviceId'] = AppConfig.instance.udid;
     params['bundleId'] = AppConfig.instance.package.packageName;
     params['version'] = AppConfig.instance.package.version;
-    params['ts'] = '${DateTime.now().millisecondsSinceEpoch}';
+    params['ts'] = ts;
     String json = jsonEncode(params);
     String crypted = _AesCryptUtils.encrypt(json);
     options.path = '/';
     options.data = crypted;
+    List<String> keys = options.headers.keys
+        .where((element) => element.startsWith('x-app'))
+        .toList();
+    keys.sort((a, b) => a.compareTo(b));
+    String val = '';
+    for (var key in keys) {
+      val += options.headers[key];
+    }
+    val += ts;
+    options.headers['x-app-checksum'] =
+        hex.encode(md5.convert(const Utf8Encoder().convert(val)).bytes);
     return super.onRequest(options, handler);
   }
 
